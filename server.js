a4bceaefedfa0b35acf88a8a8153aa4661a2804c
Eduardo Borges <eduardo_borges98@hotmail.com>
2020-05-08 18:20:35 -0300

    Final

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
-// var PORT = process.env.PORT || 5000
-// var express = require('express')
-// var path = require('path')
-// var serveStatic = require('serve-static')
+const {createServer} = require('http');
+const express= require('express');
+const compression = require('compression');
+const morgan = require('morgan');
+const path = require('path');
+const normalizePort = port => parseInt(port, 10);
+const PORT = normalizePort(process.env.PORT || 5000);
 
-// var app = express()
-// app.use(serveStatic(path.join(__dirname, 'dist')))
+const app = express();
+const dev = app.get('env') !== 'production'
 
-var PORT = process.env.PORT || 5000;
-var express = require('express');
-var app = express();
+if(!dev){
+    app.disable('x-powered-by');
+    app.use(compression());
+    app.use(morgan('common'));
 
-var http = require('http');
-var server = http.Server(app);
+    app.use(express.static(path.resolve(__dirname, 'build')));
 
-app.use(express.static('client'));
+    app.get('*', ('req', res,) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
+}
+if(dev){
+    app.use(morgan('dev'))
+}
 
+const server = createServer(app)
 
-app.listen(PORT)
-console.log('server started ' + PORT)
+server.listen(PORT, err => {
+    if(err) throw err;
+    console.log('Server Started');
+}) 
