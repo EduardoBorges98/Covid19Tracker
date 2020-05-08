cc1df3212abbab0877e72ddd559134c70d2f14a0
Eduardo Borges <eduardo_borges98@hotmail.com>
2020-05-08 17:18:36 -0300

    Deletando Procfile

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var port = process.env.PORT || 5000
+var PORT = process.env.PORT || 5000
 var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
@@ -7,5 +7,5 @@ var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
 
-app.listen(port)
-console.log('server started ' + port)
+app.listen(PORT)
+console.log('server started ' + PORT)
