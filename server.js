393f3efac2bae13d6f5d325c6dd5d20157a38469
Eduardo Borges <eduardo_borges98@hotmail.com>
2020-05-08 16:47:43 -0300

    Procfile e Server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var port = process.env.PORT || 5000
 var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
@@ -5,6 +6,6 @@ var serveStatic = require('serve-static')
 var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
-var port = process.env.PORT || 3000
+
 app.listen(port)
 console.log('server started ' + port)
