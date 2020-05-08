a35d9f601457d5b79749385f83b2a2ed4c2269e6
Eduardo Borges <eduardo_borges98@hotmail.com>
2020-05-08 18:41:09 -0300

    Covid19 Final

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ if(!dev){
 
     app.use(express.static(path.resolve(__dirname, 'build')));
 
-    app.get('*', ('req', res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html')),
-    });
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    })
 }
 if(dev){
     app.use(morgan('dev'))
