f81196cc65d3b49a29e070a1429615b81f5f6f5e
Eduardo Borges <eduardo_borges98@hotmail.com>
2020-05-08 17:41:29 -0300

    Covid19 Aplicação

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-var PORT = process.env.PORT || 5000
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+// var PORT = process.env.PORT || 5000
+// var express = require('express')
+// var path = require('path')
+// var serveStatic = require('serve-static')
 
-var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+// var app = express()
+// app.use(serveStatic(path.join(__dirname, 'dist')))
+
+var PORT = process.env.PORT || 5000;
+var express = require('express');
+var app = express();
+
+var http = require('http');
+var server = http.Server(app);
+
+app.use(express.static('client'));
 
 
 app.listen(PORT)
